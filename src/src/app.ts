8246cd7647b4558f1c8da82253623f6e4a86853d
JessyCat92 <jessica.weber.92@outlook.de>
2024-05-28 19:26:23 +0200

    Initialized the application data source in app.ts

--- src/src/app.ts
+++ src/src/app.ts
@@ -1,10 +1,11 @@
 import {ApplicationStates, setApplicationState} from "./services/ApplicationStateService";
 import {initExpress} from "./api/express";
+import {initAppDataSource} from "./data-source";
 
 
 async function startApplication() {
 
-    // await initAppDataSource();
+    await initAppDataSource();
 
     initExpress(); // set express to port 5000
 
